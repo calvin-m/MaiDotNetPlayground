6a3158c13e4f43011127c2cb97fa01c9620b04e2
Calvin Mai <Calvin.Mai@antechdx.com>
2025-06-30 16:35:51 -0700

    Added Azure Function plumbing (NuGet packages)

---
+++ MaiDotNetPlayground/AzureApps/README.md
@@ -0,0 +1,33 @@
+﻿# Azure Apps examples
+Azure enabled apps.
+
+
+
+## Azure Function Project structure
+A .NET project for Azure Functions using the isolated worker model is basically a .NET console app project that targets a supported .NET runtime. The following are the basic files required in any .NET isolated project:
+
+- C# project file (.csproj) that defines the project and dependencies.
+- Program.cs file that's the entry point for the app.
+- Any code files defining your functions.
+- host.json file that defines configuration shared by functions in your project.
+- local.settings.json file that defines environment variables used by your project when run locally on your machine.
+
+## NuGet Packages
+
+### Azure Basic packages
+- Azure.Identity
+- Microsoft.Azure.AppConfiguration.Functions.Worker  
+  - Microsoft.Azure.AppConfiguration.AspNetCore is designed for ASP.NET Core applications, while Microsoft.Azure.AppConfiguration.Functions.Worker is intended for use with Azure Functions (specifically, the .NET Isolated worker model).
+
+### Azure Function Core packages
+[Azure Function Documentation](https://learn.microsoft.com/en-us/azure/azure-functions/dotnet-isolated-process-guide?tabs=hostbuilder%2Cwindows)
+
+The following packages are required to run your .NET functions in an isolated worker process:
+
+- Microsoft.Azure.Functions.Worker
+- Microsoft.Azure.Functions.Worker.Sdk
+
+### Other Packags for Azure Functions
+
+- Microsoft.Azure.Functions.Worker.Extensions.Http.AspNetCore
+
--- MaiDotNetPlayground/Program.cs
+++ MaiDotNetPlayground/Program.cs
@@ -1,8 +1,67 @@
-﻿using MaiDotNetPlayground.DeepDiveOnLinq;
+﻿using Azure.Identity;
+using MaiDotNetPlayground.DeepDiveOnLinq;
 using MaiDotNetPlayground.ProofOfConcept.FtpClients;
 using MaiDotNetPlayground.WritingAsyncAwaitFromScratch;
+using Microsoft.Extensions.Azure;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
+
+var defaultAzureCredential = new DefaultAzureCredential(); // Azure.Identity NuGet package
+
+var host = new HostBuilder()
+    .ConfigureAppConfiguration((_, builder) =>
+    {
+        var endpoint = Environment.GetEnvironmentVariable("AppConfig");
+        if (!string.IsNullOrWhiteSpace(endpoint))
+        {
+            builder.AddAzureAppConfiguration(options =>   // Microsoft.Azure.AppConfiguration.Functions.Worker NuGet package
+            {
+                options.Connect(new Uri(endpoint), defaultAzureCredential);
+                options.ConfigureKeyVault(kvOptions => { kvOptions.SetCredential(defaultAzureCredential); });  // Microsoft.Extensions.Configuration.AzureAppConfiguration NuGet package
+            });
+        }
+    })
+    .ConfigureFunctionsWebApplication()  // Microsoft.Azure.Functions.Worker.Extensions.Http.AspNetCore
+    .ConfigureServices((hostContext, services) =>
+    {
+        //var aiOptions = new ApplicationInsightsServiceOptions
+        //{
+        //    EnableAdaptiveSampling = false,
+        //};
+        //services.AddApplicationInsightsTelemetryWorkerService(aiOptions);
+        //services.ConfigureFunctionsApplicationInsights();
+
+        //services.AddAzureClients(clientBuilder =>
+        //{
+        //    var serviceBusConnectionString = hostContext.Configuration.GetValue<string>("ServiceBusConnectionString");
+        //    var serviceBusMaxRetries = hostContext.Configuration.GetValue<int>("ServiceBusMaxRetries");
+        //    clientBuilder.AddServiceBusClient(serviceBusConnectionString).ConfigureOptions(options =>
+        //    {
+        //        //options.TransportType = ServiceBusTransportType.AmqpWebSockets;
+        //        options.RetryOptions.MaxRetries = serviceBusMaxRetries;
+        //    });
+
+        //    var blobServiceConnectionString = hostContext.Configuration.GetValue<string>("BlobStorageConnectionString");
+        //    clientBuilder.AddBlobServiceClient(blobServiceConnectionString);
+
+        //    clientBuilder.UseCredential(defaultAzureCredential);
+
+        //    //RegisterServiceBusSenders(hostContext, clientBuilder);
+        //});
+
+        //services
+        //    .AddCommonServices()
+        //    .AddEihOptions()
+        //    .AddAuthServices()
+        //    .AddDomainSpecificServices();
+    })
+    .Build();
+
+await host.RunAsync();
+return;
+
+/* Console App Entrypoint
 // For adding appsetttings.json, need to add the following 3 NuGet packages:
 // 1. Microsoft.Extensions.Configuration.Json
 // 2. Microsoft.Extensions.Configuration
@@ -15,3 +74,4 @@ SshNetFtpClient ftpClient = new(config);
 await ftpClient.ListWorkingDirectory();
 
 Console.ReadLine();
+*/
